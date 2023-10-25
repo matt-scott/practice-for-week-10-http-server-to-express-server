5cca1140348381d477f1588513c717c74562257e
Matt Scott <mattscott251@gmail.com>
2023-10-25 09:31:51 -0500

    completed project

--- server/app.js
+++ server/app.js
@@ -3,7 +3,11 @@
 // const server = http.createServer((req, res) => {
 
 // STEP 1: Import and initialize server, configure for JSON requests
-// Your code here
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
 
 // HTTP Server: Handle route with param (/users/:userId) sending plain-text response
 //     let reqBody = "";
@@ -39,11 +43,26 @@
 // });
 
 // STEP 2: Handle route with param (/users/:userId) sending plain-text response
-// Your code here
+app.get('/users/:userId', (req, res) => {
+    const userId = req.params.userId;
+    res.status(200).send(`User details for userId: ${userId}`);
+})
+
+app.get('/users', (req, res) => {
+    res.status(200).send(`User Page`);
+})
 
 // HTTP Server: Set port and listen for requests
 // const port = 5000;
 // server.listen(port, () => console.log('Server is listening on port', port));
 
 // STEP 1: Set port and listen for requests
-// Your code here
+const port = 5000;
+app.listen(port, () => console.log('Server is listening on port', port));
+
+
+
+
+
+
+
